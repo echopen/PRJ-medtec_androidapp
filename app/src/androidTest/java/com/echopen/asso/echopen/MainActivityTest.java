7156eca3e6ecb4a6567aff545bddd0aff4e650d8
benchoufi <benchoufi.mehdi@gmail.com>
2016-06-28 12:47:21 +0200

    at last, beginning the testing, instrumenting ones, here comes first UI of the MainActivity, long process to go

--- app/src/androidTest/java/com/echopen/asso/echopen/MainActivityTest.java
+++ app/src/androidTest/java/com/echopen/asso/echopen/MainActivityTest.java
@@ -1,32 +1,50 @@
 package com.echopen.asso.echopen;
 
+import android.app.Application;
+import android.app.Instrumentation;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.os.PowerManager;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.robotium.solo.Solo;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by mehdibenchoufi on 27/07/15.
  */
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private MainActivity mMainActivityTest;
-
-    public MainActivityTest() {
-        super(MainActivity.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mMainActivityTest = getActivity();
-    }
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest extends android.support.test.runner.AndroidJUnitRunner {
+    
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
 
     @Test
-    public void testInitialAlwaysPasses(){
-        assert true;
-    }
-
-    public void testPreconditions() {
-        assertNull("mMainActivityTest is null", mMainActivityTest);
+    public void checkIfMainViewsExists() throws NoMatchingViewException {
+        onView(withId(R.id.btnCapture)).check(doesNotExist());
+        onView(withId(R.id.btnEffect)).check(doesNotExist());
+        onView(withId(R.id.btnPic)).check(doesNotExist());
+        onView(withId(R.id.tabBrightness)).check(doesNotExist());
+        onView(withId(R.id.tabGrid)).check(doesNotExist());
+        onView(withId(R.id.tabSetting)).check(doesNotExist());
+        onView(withId(R.id.tabSuffle)).check(doesNotExist());
+        onView(withId(R.id.tabTime)).check(doesNotExist());
+        onView(withId(R.id.btn1)).check(doesNotExist());
+        onView(withId(R.id.btn2)).check(doesNotExist());
+        onView(withId(R.id.btn3)).check(doesNotExist());
+        onView(withId(R.id.btn4)).check(doesNotExist());
+        onView(withId(R.id.btn5)).check(doesNotExist());
     }
 }
