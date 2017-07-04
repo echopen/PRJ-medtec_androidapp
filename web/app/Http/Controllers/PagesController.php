ef962b04b13c37565a3d045c9ca0a76a1181c8cf
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-07-04 18:54:43 +0200

    Add Scan model

--- web/app/Http/Controllers/PagesController.php
+++ web/app/Http/Controllers/PagesController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Scan;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function home(){
+
+    	$scans = Scan::first();
+
+    	dd($scans);
+
 		return view('home');
 	}
 
---
+++ web/database/migrations/2017_07_04_164756_create_scans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scans', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('user_id');
+			$table->string('img_src');
+			$table->string('title');
+			$table->string('comment');
+			$table->boolean('public');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scans');
+    }
+}
