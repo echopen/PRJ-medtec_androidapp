1b937265ae0145d5346d4d8e791d9e19087c13b5
Jean-baptiste Agostin <Boss@the-world.local>
2017-07-04 16:23:00 +0200

    new activity

---
+++ android-app/app/src/main/java/com/echopen/asso/echopen/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.echopen.asso.echopen;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
