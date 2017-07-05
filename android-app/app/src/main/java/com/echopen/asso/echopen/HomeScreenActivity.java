cdcf0c16f9eb6c43dc26fcfce23bbf4cdb2b7b91
damiemoulin <damien.moulin1@gmail.com>
2017-07-05 10:37:42 +0200

    Create EchoActivity

--- android-app/app/src/main/java/com/echopen/asso/echopen/HomeScreenActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/HomeScreenActivity.java
@@ -1,19 +1,9 @@
 package com.echopen.asso.echopen;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
-import android.widget.ImageView;
-
-import com.echopen.asso.echopen.echography_image_streaming.EchographyImageStreamingService;
-import com.echopen.asso.echopen.echography_image_streaming.modes.EchographyImageStreamingMode;
-import com.echopen.asso.echopen.echography_image_streaming.modes.EchographyImageStreamingTCPMode;
-import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVisualisationContract;
-import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVisualisationPresenter;
 
 // Probe imports
 
@@ -34,50 +24,6 @@ public class HomeScreenActivity extends Activity{
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
-        EchOpenApplication app = (EchOpenApplication) getApplication();
-        EchographyImageStreamingService stream = app.getEchographyImageStreamingService();
-
-        EchographyImageVisualisationPresenter presenter = new EchographyImageVisualisationPresenter(stream, new EchographyImageVisualisationContract.View(){
-            @Override
-            public void refreshImage(final Bitmap iBitmap){
-                try{
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            ImageView echoImage = (ImageView) findViewById(R.id.echography);
-
-                            Display display = getWindowManager().getDefaultDisplay();
-                            Point size = new Point();
-                            display.getSize(size);
-                            int width = size.x;
-                            int height = size.y;
-
-
-                            echoImage.setImageBitmap(iBitmap);
-                            echoImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-                            System.out.println("Bitmap received");
-                            Log.d("Debug",iBitmap.getHeight() + "");
-                            Log.d("Debug",iBitmap.getWidth() + "");
-                        }
-                    });
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void setPresenter(EchographyImageVisualisationContract.Presenter presenter){
-
-            }
-        });
-
-        EchographyImageStreamingMode mode = new EchographyImageStreamingTCPMode("10.6.200.128", 7538);
-
-        stream.connect(mode,this);
-        presenter.start();
-        startHome();
     }
 
     /**
@@ -90,9 +36,6 @@ public class HomeScreenActivity extends Activity{
 
     public boolean onTouchEvent(MotionEvent touchevent) {
 
-        if (touchevent.getAction() == MotionEvent.ACTION_MOVE) {
-            Log.d("move", "move");
-        }
         switch (touchevent.getAction()) {
             case MotionEvent.ACTION_DOWN: {
                 x1 = touchevent.getX();
