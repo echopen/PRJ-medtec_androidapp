c58a3f7f58bab87e5adc4ca16826636892fb3e5a
Ivan Prunier <ivan.prunier@mevia.fr>
2017-07-05 16:36:52 +0200

    Adds snapshots and videos (currently clientId is fixed to 1)

--- android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.echopen.asso.echopen.echography_image_streaming.EchographyImageStreamingService;
@@ -42,8 +44,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //-----------------------------------------------------------------
+        final imagesHandler imagesHandler = new imagesHandler(getFilesDir());
 
-        EchOpenApplication app = (EchOpenApplication) getApplication(); // A caster en EchOpenApplication
+        final Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                imagesHandler.startRecording();
+            }
+        });
+
+        final Button btnEnd = (Button) findViewById(R.id.btnEnd);
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                imagesHandler.endRecording(1);
+            }
+        });
+
+        final ImageView image = (ImageView) findViewById(R.id.imageView);
+
+        EchOpenApplication app = (EchOpenApplication) getApplication();
         EchographyImageStreamingService stream = app.getEchographyImageStreamingService();
 
         EchographyImageVisualisationPresenter presenter = new EchographyImageVisualisationPresenter(stream, new EchographyImageVisualisationContract.View() {
@@ -53,13 +72,8 @@ public class MainActivity extends Activity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            ImageView image = (ImageView) findViewById(R.id.imageView);
-                            filesHandler fileHandler = new filesHandler(getFilesDir());
-
-                            fileHandler.saveCacheImage(iBitmap);
+                            imagesHandler.saveCacheImage(iBitmap);
                             image.setImageBitmap(iBitmap);
-
-                            //fileHandler.saveImage(1);
                         }
                     });
                 }
