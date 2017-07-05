6418745a2e0b1025b550aa518b6d667946bb63bd
Guillaume Etcheverry <guillaume.etcheverry28@gmail.com>
2017-07-05 10:03:42 +0200

    Fix display

--- android-app/app/src/main/java/com/echopen/asso/echopen/HomeActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class HomeActivity extends Activity implements View.OnClickListener {
@@ -12,6 +13,7 @@ public class HomeActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
 
         Button btn = (Button) findViewById(R.id.btn);
