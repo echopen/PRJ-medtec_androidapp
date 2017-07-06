8fb7c2b273af5cd37bcaefbec004f5af6aaad7ec
DinosaureMuffin <noelbilleau@gmail.com>
2017-07-06 14:04:23 +0200

    add seekBar

--- android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
@@ -8,14 +8,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.view.MotionEvent;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.View;
 import android.widget.ImageView;
 import android.view.MotionEvent;
 
@@ -25,8 +22,10 @@ import com.echopen.asso.echopen.echography_image_streaming.modes.EchographyImage
 import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVisualisationContract;
 import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVisualisationPresenter;
 
+import static android.widget.Toast.LENGTH_SHORT;
 import static com.echopen.asso.echopen.utils.Constants.Http.LOCAL_IP;
 import static com.echopen.asso.echopen.utils.Constants.Http.REDPITAYA_PORT;
+import static java.lang.Math.abs;
 
 /**
  * MainActivity class handles the main screen of the app.
@@ -39,7 +38,7 @@ import static com.echopen.asso.echopen.utils.Constants.Http.REDPITAYA_PORT;
  * These two methods should be refactored into one
  */
 
-public class MainActivity extends Activity implements View.OnTouchListener {
+public class MainActivity extends Activity {
 
     /**
      * This method calls all the UI methods and then gives hand to  UDPToBitmapDisplayer class.
@@ -107,6 +106,93 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
         });
 
+        final GestureDetector gdOnView = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener(){
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                Log.d("log", "test"+ e);
+                return true;
+            }
+
+            boolean toClose = false;
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                Log.d("tap", "OnDoubleTap" + e);
+                if (e.getAction() == MotionEvent.ACTION_DOWN && !toClose) {
+                    Log.d("tap", "toClose if " + toClose);
+                    View seekBarH = (View) findViewById(R.id.seekBarH);
+                    seekBarH.setVisibility(View.VISIBLE);
+                    View freqText = (View) findViewById(R.id.frequence);
+                    freqText.setVisibility(View.VISIBLE);
+
+                    View seekBarV = (View) findViewById(R.id.seekBarV);
+                    seekBarV.setVisibility(View.VISIBLE);
+                    View gainText = (View) findViewById(R.id.gain);
+                    gainText.setVisibility(View.VISIBLE);
+                    toClose = true;
+                } else if (e.getAction() == MotionEvent.ACTION_DOWN && toClose) {
+                    Log.d("tap", "toClose else " + toClose);
+                    View seekBarH = (View) findViewById(R.id.seekBarH);
+                    seekBarH.setVisibility(View.INVISIBLE);
+                    View freqText = (View) findViewById(R.id.frequence);
+                    freqText.setVisibility(View.INVISIBLE);
+
+
+                    View seekBarV = (View) findViewById(R.id.seekBarV);
+                    seekBarV.setVisibility(View.INVISIBLE);
+                    View gainText = (View) findViewById(R.id.gain);
+                    gainText.setVisibility(View.INVISIBLE);
+
+                    toClose = false;
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                Log.d("tap","onSingleTapConfirmed"+e);
+
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1,MotionEvent e2,float velocityX,float velocityY) {
+
+                float v = e1.getX() - e2.getX();
+                Log.d("tap", "onFling e2 "+ v );
+
+                //si taille de la barre + start event > taille window : barre stop border screen
+
+                if (abs(e1.getX() - e2.getX()) > 300 && abs(e1.getY() - e2.getY()) < 300) {
+                    //event horizontal
+                    Log.d("tap", "onFling horizontal ");
+                    //graduation 3, fréquence
+
+                }
+
+                if (abs(e1.getX() - e2.getX()) < 300 && abs(e1.getY() - e2.getY()) > 300) {
+                    //event vertical
+                    Log.d("tap", "onFling vertical ");
+                    //graduation 4, gain
+                }
+
+                return true;
+            }
+
+
+        });
+
+        View mainView = (View) findViewById(R.id.main);
+        mainView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                return gdOnView.onTouchEvent(event);
+            }
+        });
+
+
         Button mainButton = (Button) findViewById(R.id.button2);
         mainButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -131,11 +217,6 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
     boolean firstTouch = false;
     long time = System.currentTimeMillis();
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        this.tapOnView(event.getAction());
-        return true;
-    }
 
 
 
@@ -173,11 +254,5 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        this.tapOnView (motionEvent.getAction());
-        return false;
     }
 }
