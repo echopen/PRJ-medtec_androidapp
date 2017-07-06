b043d04d4e647656a94bbbee9ed7863e5c9164ec
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-07-06 23:43:49 +0200

    [ADD] First Img View in Gallery

--- android-app/app/src/main/java/com/echopen/asso/echopen/fragments/GalleryFragment.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/fragments/GalleryFragment.java
@@ -1,13 +1,21 @@
 package com.echopen.asso.echopen.fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.echopen.asso.echopen.R;
+import com.echopen.asso.echopen.bdd.Image;
+import com.echopen.asso.echopen.bdd.ImageDAO;
+import com.echopen.asso.echopen.utils.ImageService;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,4 +35,18 @@ public class GalleryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_gallery2, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ImageView firstImg = (ImageView) getActivity().findViewById(R.id.imgView_first);
+        TextView textView = (TextView) getActivity().findViewById(R.id.gallery);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Lato-Bold.ttf");
+        textView.setTypeface(typeface);
+
+        ImageDAO imageDAO = new ImageDAO(getContext());
+        imageDAO.open();
+        Image image = imageDAO.getLastImg();
+        Bitmap bitmap = ImageService.loadImageFromStorage(image.getImgName());
+        firstImg.setImageBitmap(bitmap);
+    }
 }
--- android-app/app/src/main/java/com/echopen/asso/echopen/fragments/SettingsFragment.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/fragments/SettingsFragment.java
@@ -1,11 +1,14 @@
 package com.echopen.asso.echopen.fragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.echopen.asso.echopen.R;
 
@@ -25,6 +28,14 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView textView = (TextView) getActivity().findViewById(R.id.param);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Lato-Bold.ttf");
+        textView.setTypeface(typeface);
     }
 
 }
