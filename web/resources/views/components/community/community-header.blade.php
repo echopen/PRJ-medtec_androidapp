a3c5f3c73afb2b1913ef49651d95ef74b8d8b0fb
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-07-06 23:48:52 +0200

    Edit guest page and edit linkedin name

--- web/resources/views/components/community/community-header.blade.php
+++ web/resources/views/components/community/community-header.blade.php
@@ -8,4 +8,220 @@
         <button class="submit">RECHERCHER</button>
         <div class="magnifier"></div>
     </div>
+
+
+    <div class="filter">
+    </div>
+
+
+    <div class="scans">
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scan">
+            <div class="top-image"></div>
+            <div class="text-content">
+                <div class="date-type">Le 23 Juin 2017 <span>|</span>Echographie</div>
+                <div class="title-scan">Hydronephrosis</div>
+                <div class="description">L'hydronéphrose est généralement liée à un obstacle (passager ou permanent), ou à un manque de tonicité (force) du bassinet.</div>
+            </div>
+            <div class="profile">
+                <div class="profile-container">
+                    <div class="left-content">
+                        <div class="picture"></div>
+                        <div class="texts">
+                            <span class="name">Philippe</span><br>
+                            <img src="/img/community/position.png" alt=""><span>Florence, Italie</span>
+                        </div>
+                    </div>
+                    <div class="right-content">
+                        <img src="/img/community/heart.png" alt="">
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </section>
