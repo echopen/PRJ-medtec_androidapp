f9da9df0563ce25c009de99ceb82d8496d67cee7
Stan <stanislas.manscour@hetic.net>
2017-07-06 11:30:56 +0200

    section numbers & scroll

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -5,11 +5,15 @@
         @include('components.home_presentation')
     </section>
     
+    <section class="base_section base_section-numbers">
+        @include('components.home_numbers')
+    </section>
+
     <section class="base_section base_section-faq">
        @include('components.home_faq')
     </section>
     
-     
+    
     <section class="base_section base_section-community">
        @include('components.home_community')
     </section>
