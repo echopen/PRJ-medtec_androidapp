f135cae0adc8c408495fe3e4f6419ec3dea2ae75
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-07-06 13:37:40 +0200

    Edit FAQ and add contact page template

--- web/resources/views/faq.blade.php
+++ web/resources/views/faq.blade.php
@@ -12,19 +12,19 @@
             <p class="description">sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
         </div>
         <div class="question">
-            <p class="title">Comment enregistrer mes images</p>
+            <p class="title">L’application est-elle compatible iOS ?</p>
             <p class="description">sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
         </div>
         <div class="question">
-            <p class="title">Comment enregistrer mes images</p>
+            <p class="title">Comment prendre une mesure sur mon échographie ?</p>
             <p class="description">sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
         </div>
         <div class="question">
-            <p class="title">Comment enregistrer mes images</p>
+            <p class="title">Comment modifier les paramètres d’affichage pendant l’utilisation ?</p>
             <p class="description">sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
         </div>
         <div class="question">
-            <p class="title">Comment enregistrer mes images</p>
+            <p class="title">Comment modifier les paramètres de mesures pendant l’utilisation ?</p>
             <p class="description">sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
         </div>
     </div>
--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -4,28 +4,18 @@
     <section class="welcome">
         @include('components.home_presentation')
     </section>
- 
     @include('components.home_nl')
-
     @include('components.home_history')
-
     @include('components.home.community')
-  
+    <section class="base_section base_section-howItWorks">
+        @include('components.home_howItWorks')
+    </section>
     @include('components.home_nl')
-
-    @include('components.home_faq')
-    
-    @include('components.home_community')
     <section class="base_section base_section-numbers">
         @include('components.home_numbers')
     </section>
-
-
-     <section class="base_section base_section-howItWorks">
-       @include('components.home_howItWorks')
-    </section>
-
-
+    @include('components.home_faq')
+    @include('components.home_community')
     @include('components.home.partners')
 
 @endsection
