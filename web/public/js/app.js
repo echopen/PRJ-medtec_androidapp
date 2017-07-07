6d0bb8b799baf842ddf76ce3c1644ebf882f0d06
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-07-07 07:59:28 +0200

    Edit community header

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1701,29 +1701,41 @@ if (token) {
 /***/ (function(module, exports) {
 
 $(document).ready(function () {
-  $('.entry_slick-multiple').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 400,
-    variableWidth: true,
-    prevArrow: "<div class='slick_btn_wrap'><button type='button' class='slick-prev pull-left'></button></div>",
-    nextArrow: "<div class='slick_btn_wrap slick_btn_wrap-left'><button type='button' class='slick-next pull-right'></button></div>"
-  });
-  $('.entry_slick-solo').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 400,
-    dots: true,
-    arrows: true,
-    variableWidth: true,
-    prevArrow: "<div class='slick_btn_wrap'><button type='button' class='slick-prev pull-left'><img src='/img/arrow.svg'></button></div>",
-    nextArrow: "<div class='slick_btn_wrap slick_btn_wrap-left'><button type='button' class='slick-next pull-right'><img src='/img/arrow.svg'></button></div>"
-  });
+    $('.entry_slick-multiple').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 400,
+        variableWidth: true
+
+    });
+    $('.entry_slick-solo').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 400,
+        dots: true,
+        arrows: true,
+        variableWidth: true
+
+    });
 });
 $(window).ready(function () {
-  setTimeout(function () {
-    $(' .slick-current').css("margin-left", "30px");
-  }, 300); //pas le choix pour afficher shadow du premier enfant
+    setTimeout(function () {
+        $(' .slick-current').css("margin-left", "30px");
+    }, 300); //pas le choix pour afficher shadow du premier enfant
+
+    $('.base_section-faq .slick_controls .slick_left').click(function () {
+        $(".entry_slick-multiple").slick('slickPrev');
+    });
+    $('.base_section-faq .slick_controls .slick_right').click(function () {
+        $(".entry_slick-multiple").slick('slickNext');
+    });
+
+    $('.base_section-history .slick_controls .slick_left').click(function () {
+        $(".entry_slick-solo").slick('slickPrev');
+    });
+    $('.base_section-history .slick_controls .slick_right').click(function () {
+        $(".entry_slick-solo").slick('slickNext');
+    });
 });
 
 /***/ }),
