29f108cbf72b730b0b37d73a0a8ed01febaa9195
Nicocco <nicolas.morisseau53@gmail.com>
2018-02-15 09:20:48 +0100

    Correction de bug

--- probe_emulator_java/srcbin/Main.java
+++ probe_emulator_java/srcbin/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Serveur ImageServer = new Serveur();
-		//Serveur_Bat BatServer = new Serveur_Bat(Integer.valueOf(10)); // value send
+		//Serveur ImageServer = new Serveur();
+		Serveur_Bat BatServer = new Serveur_Bat(Integer.valueOf(args[0])); // value send
 	
 		
 	}
