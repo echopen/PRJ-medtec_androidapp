29a283f5fa7237632bdf3b2b78f2405d62ed8d7e
Yvan Moté <yvan.mote@gmail.com>
2018-02-28 14:41:46 +0100

    Refactor done with CaptureButton on MainActivity.

--- android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
@@ -23,6 +23,7 @@ import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVi
 import com.echopen.asso.echopen.echography_image_visualisation.EchographyImageVisualisationPresenter;
 import com.echopen.asso.echopen.filters.RenderingContext;
 import com.echopen.asso.echopen.utils.Constants;
+import com.echopen.asso.echopen.view.CaptureButton;
 
 /**
  * MainActivity class handles the main screen of the app.
@@ -45,11 +46,7 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
     private ImageView mEndExamButton;
     private ImageView mBatteryButton;
     private ImageView mSelectButton;
-    private ImageView mCaptureShadow;
-
-
-    private Long then;
-    private RotateAnimation rotate_animation_capture;
+    private CaptureButton mCaptureShadow;
 
     private final static float IMAGE_ZOOM_FACTOR = 1.75f;
     private final static float IMAGE_ROTATION_FACTOR = 90.f;
@@ -74,56 +71,29 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
 
         setContentView(R.layout.activity_main);
 
-        mCaptureButton = (ImageView) findViewById(R.id.main_button_capture);
+        mCaptureButton = findViewById(R.id.main_button_capture);
 
-        mCaptureShadow = (ImageView) findViewById(R.id.main_button_shadow);
+        mCaptureShadow = findViewById(R.id.main_button_shadow);
         mCaptureShadow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("captureButton", "Short Press");
-
-
             }
         });
-        mCaptureShadow.setOnTouchListener(new View.OnTouchListener() {
+
+        mCaptureShadow.setListener(new CaptureButton.CaptureButtonListener() {
             @Override
+            public void onTouchDown() {
+                mEchographyImageVisualisationPresenter.toggleFreeze();
+            }
 
-            public boolean onTouch(View v,final MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    then = System.currentTimeMillis();
-                   mEchographyImageVisualisationPresenter.toggleFreeze();
-
-                    rotate_animation_capture = new RotateAnimation(0,144 ,
-                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                            0.6f);
-
-                    rotate_animation_capture.setDuration(5000);
-                    mCaptureShadow.clearAnimation();
-                    mCaptureShadow.setAnimation(rotate_animation_capture);
-
-                }
-
-
-                else if(event.getAction() == MotionEvent.ACTION_UP){
-                    mEchographyImageVisualisationPresenter.toggleFreeze();
-                    if(((Long) System.currentTimeMillis() - then) > 5000){
-                        Log.d("mcaptureButton", "Long Press");
-
-                        return true;
-                    }
-                    else {
-
-                        rotate_animation_capture.cancel();
-                        rotate_animation_capture= null;
-
-                    }
-                }
-                return false;
+            @Override
+            public void onTouchUp() {
+                mEchographyImageVisualisationPresenter.toggleFreeze();
             }
         });
 
-
-        mPregnantWomanButton = (ImageView) findViewById(R.id.main_button_mode);
+        mPregnantWomanButton = findViewById(R.id.main_button_mode);
         mPregnantWomanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -131,7 +101,7 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
             }
         });
 
-        mSelectButton = (ImageView) findViewById(R.id.main_button_select);
+        mSelectButton = findViewById(R.id.main_button_select);
         mSelectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -139,7 +109,7 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
             }
         });
 
-        mEndExamButton = (ImageView) findViewById(R.id.main_button_end_exam);
+        mEndExamButton = findViewById(R.id.main_button_end_exam);
         mEndExamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -147,7 +117,7 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
             }
         });
 
-        mBatteryButton = (ImageView) findViewById(R.id.main_button_battery);
+        mBatteryButton = findViewById(R.id.main_button_battery);
         mBatteryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -175,9 +145,6 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-
-
-
     @Override
     public void refreshImage(final Bitmap iBitmap) {
         this.runOnUiThread(new Runnable() {
@@ -197,14 +164,11 @@ public class MainActivity extends Activity implements EchographyImageVisualisati
    public void displayFreezeButton() {
         mCaptureShadow.setImageResource(R.drawable.icon_arc_shadow);
         mCaptureButton.setImageResource(R.drawable.button_jauge);
-
     }
 
     @Override
     public void displayUnfreezeButton() {
         mCaptureShadow.setImageResource(R.drawable.icon_save_image);
-
-
     }
 
     @Override
---
+++ android-app/app/src/main/java/com/echopen/asso/echopen/view/CaptureButton.java
@@ -0,0 +1,86 @@
+package com.echopen.asso.echopen.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+
+/**
+ * Created by Yvan Moté on 28/02/2018.
+ */
+
+public class CaptureButton extends android.support.v7.widget.AppCompatImageButton {
+
+    public interface CaptureButtonListener {
+        void onTouchDown();
+        void onTouchUp();
+    }
+
+    private RotateAnimation rotateAnimationCapture;
+    private Long then;
+    private CaptureButtonListener listener;
+
+    private long animationDuration = 5000l;
+
+    public CaptureButton(Context context) {
+        super(context);
+    }
+
+    public CaptureButton(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CaptureButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setListener(CaptureButtonListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            then = System.currentTimeMillis();
+
+            if(listener!=null) {
+                listener.onTouchDown();
+            }
+
+            rotateAnimationCapture = new RotateAnimation(0,144 ,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                    0.6f);
+            rotateAnimationCapture.setDuration(animationDuration);
+
+            this.clearAnimation();
+            this.setAnimation(rotateAnimationCapture);
+
+            return true;
+        } else if(event.getAction() == MotionEvent.ACTION_UP) {
+
+            if(listener!=null) {
+                listener.onTouchUp();
+            }
+
+            if(((Long) System.currentTimeMillis() - then) > animationDuration){
+                Log.d("mcaptureButton", "Long Press");
+
+                return true;
+            }
+            else {
+
+                Log.d("mcaptureButton", "cancel");
+
+                rotateAnimationCapture.cancel();
+                rotateAnimationCapture= null;
+
+            }
+        }
+        return false;
+    }
+}
